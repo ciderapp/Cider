42680aff79309f6e5eecac3ecedf67a02505bd1d
cryptofyre <cryptofyre@protonmail.com>
2021-11-19 17:31:43 -0600

    Edit to Cider.

--- resources/functions/media/discordrpc.js
+++ resources/functions/media/discordrpc.js
@@ -73,7 +73,7 @@ module.exports = {
             smallImageText: (attributes.status ? 'Playing': 'Paused'),
             instance: true,
             buttons: [
-                {label: "Listen on AME", url: listenURL},
+                {label: "Listen on Cider", url: listenURL},
             ]
         };
         console.verbose(`[LinkHandler] Listening URL has been set to: ${listenURL}`);
