077a164b5e9c9b6dc535911be5afb0451940ffef
cryptofyre <cryptofyre@cryptofyre.org>
2021-12-02 08:54:17 -0600

    pull in cider-ui-test stuff.

--- docs/app-win/index.js
+++ docs/app-win/index.js
@@ -1,9 +1,50 @@
 var app = new Vue({
     el: "#app",
     data: {
-        drawertest: false
+        drawertest: false,
+        mk: {},
+        quickPlayQuery: ""
     },
     methods: {
-
+        init() {
+            this.mk = MusicKit.getInstance()
+            this.$forceUpdate()
+        },
+        mkReady() {
+            if(this.mk["nowPlayingItem"]) {
+                return true
+            }else{
+                return false
+            }
+        },
+        getNowPlayingArtwork(size = 600) {
+            if (this.mk["nowPlayingItem"]["attributes"]["artwork"]["url"]) {
+                return `url("${this.mk["nowPlayingItem"]["attributes"]["artwork"]["url"].replace('{w}', size).replace('{h}', size)}")`;
+            } else {
+                return "";
+            }
+        },
+        quickPlay(query) {
+            let self = this
+            MusicKit.getInstance().api.search(query, { limit: 2, types: 'songs' }).then(function (data) {
+                MusicKit.getInstance().setQueue({ song: data["songs"]['data'][0]["id"] }).then(function (queue) {
+                    MusicKit.getInstance().play()
+                    setTimeout(()=>{
+                        self.$forceUpdate()
+                    }, 1000)
+                })
+            })
+        }
     }
 })
+
+document.addEventListener('musickitloaded', function() {
+    // MusicKit global is now defined
+    MusicKit.configure({
+        developerToken: '',
+        app: {
+            name: 'My Cool Web App',
+            build: '1978.4.1'
+        }
+    });
+});
