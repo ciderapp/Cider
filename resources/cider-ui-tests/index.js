f88477f18f9aad9517f91b352b2e95c95178c25a
booploops <49113086+booploops@users.noreply.github.com>
2021-12-03 12:41:31 -0800

    Added sidebar-library-item component

--- resources/cider-ui-tests/index.js
+++ resources/cider-ui-tests/index.js
@@ -1,22 +1,25 @@
+Vue.component('sidebar-library-item', {
+    template: '#sidebar-library-item',
+    props: ['name', 'page'],
+    methods: {}
+});
+
 Vue.component('mediaitem-square', {
     template: '#mediaitem-square',
     props: ['item'],
-    methods: {
-    }
+    methods: {}
 });
 
 Vue.component('mediaitem-hrect', {
     template: '#mediaitem-hrect',
     props: ['item'],
-    methods: {
-    }
+    methods: {}
 });
 
 Vue.component('mediaitem-list-item', {
     template: '#mediaitem-list-item',
     props: ['item'],
-    methods: {
-    }
+    methods: {}
 });
 
 const MusicKitTools = {
@@ -56,13 +59,20 @@ const app = new Vue({
             this.mk.authorize()
             this.$forceUpdate()
 
-            this.mk.addEventListener(MusicKit.Events.playbackTimeDidChange, (a)=>{
+            this.mk.addEventListener(MusicKit.Events.playbackTimeDidChange, (a) => {
                 self.playerLCD.playbackDuration = (self.mk.currentPlaybackTime)
             })
 
             this.apiCall('https://api.music.apple.com/v1/me/library/playlists', res => {
                 self.playlists.listing = res.data
             })
+        },
+        getSidebarItemClass(page) {
+            if (this.page == page) {
+                return ["active"]
+            } else {
+                return []
+            }
         },
         unauthorize() {
             this.mk.unauthorize()
@@ -71,7 +81,7 @@ const app = new Vue({
             this.page = "search"
         },
         playMediaItemById(id, kind) {
-            this.mk.setQueue({ [kind]: [id] }).then(function (queue) {
+            this.mk.setQueue({[kind]: [id]}).then(function (queue) {
                 MusicKit.getInstance().play()
             })
         },
@@ -81,14 +91,14 @@ const app = new Vue({
                 {
                     types: "songs,artists,albums,playlists",
                     limit: 32
-                }).then(function(results) {
+                }).then(function (results) {
                 self.search.results = results
             })
         },
         mkReady() {
             if (this.mk["nowPlayingItem"]) {
                 return true
-            } else{
+            } else {
                 return false
             }
         },
@@ -104,10 +114,10 @@ const app = new Vue({
         },
         quickPlay(query) {
             let self = this
-            MusicKit.getInstance().api.search(query, { limit: 2, types: 'songs' }).then(function (data) {
-                MusicKit.getInstance().setQueue({ song: data["songs"]['data'][0]["id"] }).then(function (queue) {
+            MusicKit.getInstance().api.search(query, {limit: 2, types: 'songs'}).then(function (data) {
+                MusicKit.getInstance().setQueue({song: data["songs"]['data'][0]["id"]}).then(function (queue) {
                     MusicKit.getInstance().play()
-                    setTimeout(()=>{
+                    setTimeout(() => {
                         self.$forceUpdate()
                     }, 1000)
                 })
@@ -148,26 +158,26 @@ const app = new Vue({
     }
 })
 
-document.addEventListener('musickitloaded', function() {
+document.addEventListener('musickitloaded', function () {
     // MusicKit global is now defined
     fetch("https://beta.music.apple.com/")
-    .then(response => response.text())
-    .then(data => {
-        var el = document.createElement("html");
-        el.innerHTML = data;
-        var u = el.querySelector(`[name="desktop-music-app/config/environment"]`)
-        var amwebCFG = JSON.parse(decodeURIComponent(u.getAttribute("content")));
-        console.log(amwebCFG.MEDIA_API.token)
-        MusicKit.configure({
-            developerToken: amwebCFG.MEDIA_API.token,
-            app: {
-                name: 'My Cool Web App',
-                build: '1978.4.1'
-            }
+        .then(response => response.text())
+        .then(data => {
+            var el = document.createElement("html");
+            el.innerHTML = data;
+            var u = el.querySelector(`[name="desktop-music-app/config/environment"]`)
+            var amwebCFG = JSON.parse(decodeURIComponent(u.getAttribute("content")));
+            console.log(amwebCFG.MEDIA_API.token)
+            MusicKit.configure({
+                developerToken: amwebCFG.MEDIA_API.token,
+                app: {
+                    name: 'My Cool Web App',
+                    build: '1978.4.1'
+                }
+            });
+            setTimeout(() => {
+                app.init()
+            }, 1000)
         });
-        setTimeout(()=>{
-            app.init()
-        }, 1000)
-    });
-    
+
 });
