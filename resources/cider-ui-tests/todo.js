dec21c6650e728914c1177f09e48baf03bf86137
vapormusic <vietanhfat@gmail.com>
2021-12-03 17:49:30 +0700

    some further notes for apple recommendations api

--- resources/cider-ui-tests/todo.js
+++ resources/cider-ui-tests/todo.js
@@ -1,3 +1,24 @@
+// Apple Music Listen Now Page
+// URL : https://amp-api.music.apple.com/v1/me/recommendations?timezone=+00:00
+        //  &with=friendsMix,library,social&art[social-profiles:url]=c
+        //  &name=listen-now&art[url]=c,f&omit[resource]=autos
+        //  &relate[editorial-items]=contents
+        //  &extend=editorialCard,editorialVideo
+        //  &extend[albums]=artistUrl
+        //  &extend[library-albums]=artistUrl
+        //  &extend[playlists]=artistNames,editorialArtwork
+        //  &extend[library-playlists]=artistNames,editorialArtwork
+        //  &extend[social-profiles]=topGenreNames&include[albums]=artists
+        //  &include[songs]=artists&include[music-videos]=artists
+        //  &fields[albums]=artistName,artistUrl,artwork,contentRating,editorialArtwork,editorialVideo,name,playParams,releaseDate,url
+        //  &fields[artists]=name,url&extend[stations]=airDate,supportsAirTimeUpdates&meta[stations]=inflectionPoints
+        //  &types=artists,albums,editorial-items,library-albums,library-playlists,music-movies,music-videos,playlists,stations,uploaded-audios,uploaded-videos,activities,apple-curators,curators,tv-shows,social-profiles,social-upsells
+        //  &l=en-gb&platform=web 
+// How Apple is kinda doing it :
+// MusicKit.getInstance().api.recommendations({extend: ["editorialArtwork", "artistUrl"],
+//                 "fields[artists]": ["name", "url", "artwork", "editorialArtwork", "genreNames", "editorialNotes"],
+//                 "art[url]": "f"});
+        
 // Made For You
 app.mk.api.recommendations({extend: "editorialArtwork,artistUrl"})
 
