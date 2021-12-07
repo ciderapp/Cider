fed4c240341db53462ec8cd3b9f2e33e043804a7
vapormusic <vietanhfat@gmail.com>
2021-12-07 14:00:14 +0700

    music player buttons fix

--- resources/cider-ui-tests/index.js
+++ resources/cider-ui-tests/index.js
@@ -211,8 +211,10 @@ const app = new Vue({
 
                 if (type.includes("musicVideo")){
                     document.getElementById("apple-music-video-container").style.display = "block";
+                    app.chrome.topChromeVisible = false
                 } else {
                     document.getElementById("apple-music-video-container").style.display = "none";
+                    app.chrome.topChromeVisible = true
                 }
                 self.chrome.artworkReady = false
                 self.lyrics = []
