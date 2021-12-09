ee6a3ba499b6afb62e283dc26302bab893fda458
booploops <49113086+booploops@users.noreply.github.com>
2021-12-08 23:20:33 -0800

    Adds search hints / suggestions while typing

--- resources/cider-ui-tests/index.js
+++ resources/cider-ui-tests/index.js
@@ -61,6 +61,8 @@ const app = new Vue({
         quickPlayQuery: "",
         search: {
             term: "",
+            hints: [],
+            showHints: false,
             results: {},
             limit: 10
         },
@@ -301,6 +303,13 @@ const app = new Vue({
             return {
                 'background': ('linear-gradient(to right, var(--keyColor) 0%, var(--keyColor) ' + value + '%, #333 ' + value + '%, #333 100%)')
             }
+        },
+        async getSearchHints() {
+            if(this.search.term == "") {
+                this.search.hints = ""
+            }
+            let hints = await app.mkapi("searchHints", false, this.search.term)
+            this.search.hints = hints.terms
         },
         getSongProgress() {
             if(this.playerLCD.userInteraction) {
