c2ec359f7b8ade41804972a4484763cea6132375
vapormusic <vietanhfat@gmail.com>
2021-12-17 11:13:56 +0700

    Service Workers to cache image

--- resources/cider-ui/index.js
+++ resources/cider-ui/index.js
@@ -1885,6 +1885,13 @@ document.addEventListener('musickitloaded', function () {
     request.send();
 });
 
+if ('serviceWorker' in navigator) {
+    // Use the window load event to keep the page load performant
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('sw.js?v=1');
+    });
+  }
+
 function uuidv4() {
     return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
         (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
