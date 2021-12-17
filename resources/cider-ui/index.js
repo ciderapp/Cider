08874c70ea4408a6a1254de4047e36baffc044e2
vapormusic <vietanhfat@gmail.com>
2021-12-17 21:54:49 +0700

    disable SW for now, fix lyrics, comment release for win

--- resources/cider-ui/index.js
+++ resources/cider-ui/index.js
@@ -1842,12 +1842,12 @@ document.addEventListener('musickitloaded', function () {
     request.send();
 });
 
-if ('serviceWorker' in navigator) {
-    // Use the window load event to keep the page load performant
-    window.addEventListener('load', () => {
-      navigator.serviceWorker.register('sw.js?v=1');
-    });
-  }
+// if ('serviceWorker' in navigator) {
+//     // Use the window load event to keep the page load performant
+//     window.addEventListener('load', () => {
+//       navigator.serviceWorker.register('sw.js?v=1');
+//     });
+//   }
 
 function uuidv4() {
     return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
