165fdfd5bd8b0a70491a094615bca1f6cfcd2c5f
vapormusic <vietanhfat@gmail.com>
2022-01-23 16:32:09 +0700

    do this

--- build/afterPack.js
+++ build/afterPack.js
@@ -1,12 +1,14 @@
 exports.default = function(context) {
     const { execSync } = require('child_process')
 
+    if (process.platform !== 'darwin')
+        return
+        
     console.log('Castlabs-evs update start')
     execSync('python3 -m pip install --upgrade castlabs-evs')
     console.log('Castlabs-evs update complete')
 
-    if (process.platform !== 'darwin')
-        return
+
 
     console.log('VMP signing start')
 
