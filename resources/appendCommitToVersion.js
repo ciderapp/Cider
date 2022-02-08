3d22203f949ae6c32b72b35ee99390f94d08aa81
Quacksire <19170969+quacksire@users.noreply.github.com>
2022-02-07 18:04:50 -0800

    unbork bonk

--- resources/appendCommitToVersion.js
+++ resources/appendCommitToVersion.js
@@ -13,7 +13,7 @@ var package = JSON.parse(data);
 package.version = `${package.version}.${process.env['CIRCLE_BUILD_NUM']}`
 
 
-fs.writeFile('package.json', newData, err => {
+fs.writeFile('package.json', package, err => {
     // error checking
     if(err) throw err;
     console.log("VERSION CHANGED");
