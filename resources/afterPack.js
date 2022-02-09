479e2024baaa848babf5da4fb203731577046b4f
vapormusic <vietanhfat@gmail.com>
2022-02-09 10:25:10 +0700

    fix this

--- resources/afterPack.js
+++ resources/afterPack.js
@@ -4,7 +4,8 @@ exports.default = function(context) {
 
     if (process.platform !== 'darwin')
         return
-        
+    
+    fs.unlinkSync(context.appOutDir + '/Cider.app/Contents/Frameworks/Electron Framework.framework/Versions/A/Resources/Electron Framework.sig')
     console.log('Castlabs-evs update start')
     execSync('python3 -m pip install --upgrade castlabs-evs')
     console.log('Castlabs-evs update complete')
@@ -12,6 +13,8 @@ exports.default = function(context) {
     
 
     console.log('VMP signing start')
+    if (fs.existsSync('dist/mac-universal'))
+    execSync('python3 -m castlabs_evs.vmp -n sign-pkg dist/mac-universal',{stdio: 'inherit'})
     if (fs.existsSync('dist/mac'))
     execSync('python3 -m castlabs_evs.vmp -n sign-pkg dist/mac',{stdio: 'inherit'})
     if (fs.existsSync('dist/mac-arm64'))
