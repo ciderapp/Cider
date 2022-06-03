4844fcc29f62a70ac7d606203f7e502dd26cb221
Core <64542347+coredev-uk@users.noreply.github.com>
2022-06-03 15:13:52 +0100

    this annoys me

--- resources/version.sh
+++ resources/version.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-LATESTSHA=$(curl -s https://api.github.com/repos/ciderapp/Cider/branches/stable | grep sha | cut -d '"' -f 4 | sed 's/v//' | xargs)
-COMMITSINCESTABLE=$(git rev-list $LATESTSHA..HEAD --count)
-VERSION=$(grep '"version":.*' package.json | cut -d '"' -f 4 | head -1)
-echo 
-NEWVERSION=${VERSION/-/.}-$COMMITSINCESTABLE
-npm version $NEWVERSION
+LATEST_SHA=$(curl -s https://api.github.com/repos/ciderapp/Cider/branches/stable | grep sha | cut -d '"' -f 4 | sed 's/v//' | xargs)
+COMMITSINCESTABLE=$(git rev-list $LATEST_SHA..HEAD --count)
+CURRENT_VERSION=$(node -p -e "require('./package.json').version")
+NEW_VERSION=${CURRENT_VERSION/-/.}.$COMMITSINCESTABLE
+echo "Version: $NEW_VERSION"
+sed -i "0,/$CURRENT_VERSION/s//$NEW_VERSION/" package.json
