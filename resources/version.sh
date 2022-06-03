60159617b837067013700decb405df83da3e5eba
Core <64542347+coredev-uk@users.noreply.github.com>
2022-06-03 15:18:35 +0100

    ima just comment this as its clear if its beta or not based on commit count

--- resources/version.sh
+++ resources/version.sh
@@ -4,5 +4,12 @@ LATEST_SHA=$(curl -s https://api.github.com/repos/ciderapp/Cider/branches/stable
 COMMITSINCESTABLE=$(git rev-list $LATEST_SHA..HEAD --count)
 CURRENT_VERSION=$(node -p -e "require('./package.json').version")
 NEW_VERSION=${CURRENT_VERSION/-/.}.$COMMITSINCESTABLE
-echo "Version: $NEW_VERSION"
-sed -i "0,/$CURRENT_VERSION/s//$NEW_VERSION/" package.json
+if [[ $COMMITSINCESTABLE -gt 0 ]]
+then
+  echo "Version: $NEW_VERSION"
+  sed -i "0,/$CURRENT_VERSION/s//$NEW_VERSION/" package.json
+else
+  echo "Version unchanged, commits since stable is 0"
+fi
+
+
