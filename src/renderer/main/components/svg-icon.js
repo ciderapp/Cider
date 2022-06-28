60f34e91d66d8178cf514f6d452bdad8d0938127
booploops <49113086+booploops@users.noreply.github.com>
2022-06-28 15:08:00 -0700

    fix for svg-icon size, added :classes property

--- src/renderer/main/components/svg-icon.js
+++ src/renderer/main/components/svg-icon.js
@@ -1,9 +1,13 @@
 export const svgIcon = Vue.component("svg-icon", {
     template: `
-        <div class="_svg-icon" :svg-name="name" :style="{'--icon': 'url(' + url + ')'}"></div>
+        <div class="_svg-icon" :class="classes" :svg-name="name" :style="{'--icon': 'url(' + url + ')'}"></div>
     `,
     props: {
         name: {
+            type: String,
+            required: false
+        },
+        classes: {
             type: String,
             required: false
         },
