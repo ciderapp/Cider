9b7246ea49ed223cd783868121d1439f83b7cf93
maikirakiwi <stella@mai.kiwi>
2022-07-10 17:26:31 -0700

    test getting rid of lyriccurrenttime

--- src/renderer/main/vueapp.js
+++ src/renderer/main/vueapp.js
@@ -925,7 +925,7 @@ const app = new Vue({
             })
 
             this.mk.addEventListener(MusicKit.Events.playbackTimeDidChange, (a) => {
-                self.lyriccurrenttime = self.mk.currentPlaybackTime - app.lyricOffset
+                // self.lyriccurrenttime = self.mk.currentPlaybackTime - app.lyricOffset
                 this.currentSongInfo = a
                 self.playerLCD.playbackDuration = (self.mk.currentPlaybackTime)
                 // wsapi
