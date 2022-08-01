c07ae6972ea70ba726d194d245eceb2ca79054bd
Core <64542347+coredev-uk@users.noreply.github.com>
2022-08-01 14:29:20 +0100

    Here we go again

--- resources/version.sh
+++ resources/version.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
-LATEST_SHA=$(curl -s https://api.github.com/repos/ciderapp/Cider/branches/stable | grep '"sha"' | head -1 | cut -d '"' -f 4)
-SHA_DATE=$(git show -s --format=%ci $LATEST_SHA)
-COMMITSINCESTABLE=$(git rev-list $LATEST_SHA..HEAD --count --since="$SHA_DATE")
+printenv
+# DEBUGGING
+LATEST_SHA=$(curl -s https://api.github.com/repos/ciderapp/Cider/branches/stable | grep sha | cut -d '"' -f 4 | sed 's/v//' | xargs | cut -d' ' -f1)
+echo $LATEST_SHA
+COMMITSINCESTABLE=$(git rev-list $LATEST_SHA..HEAD --count)
+echo $COMMITSINCESTABLE
 CURRENT_VERSION=$(node -p -e "require('./package.json').version")
-if [[ $CIRCLE_BRANCH == "main" && $COMMITSINCESTABLE -gt 0 ]]; then
+echo $CURRENT_VERSION
+GITHUB_BASE_REF="main"
+echo $GITHUB_BASE_REF
+
+#############################################################################################
+
+if [[ ($CIRCLE_BRANCH == "main" || $GITHUB_BASE_REF == "main") && $COMMITSINCESTABLE -gt 0 ]]; then
   NEW_VERSION="${CURRENT_VERSION}-beta.${COMMITSINCESTABLE}"
 else
   NEW_VERSION=${CURRENT_VERSION/0/$COMMITSINCESTABLE}
@@ -17,4 +26,8 @@ else
   echo "Version unchanged, commits since stable is ${COMMITSINCESTABLE}"
 fi
 
-echo "export APP_VERSION=$(node -p -e 'require("./package.json").version')" >>$BASH_ENV
+if [[ -z "${GITHUB_ENV}" ]]; then
+  echo "APP_VERSION=$(node -p -e 'require("./package.json").version')" >>$GITHUB_ENV
+elif [[ -z "${BASH_ENV}" ]]; then
+  echo "export APP_VERSION=$(node -p -e 'require("./package.json").version')" >>$BASH_ENV
+fi
