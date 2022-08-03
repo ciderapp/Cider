dbef4daade833d9766b7e1064f3bd5c77c4f2a1f
Core <64542347+coredev-uk@users.noreply.github.com>
2022-08-03 16:34:04 +0100

    Comments and echos

--- resources/version.sh
+++ resources/version.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
-printenv
-
 STABLE_SHA=$(curl -s https://api.github.com/repos/ciderapp/Cider/branches/stable | grep '"sha"' | head -1 | cut -d '"' -f 4)
 SHA_DATE=$(git show -s --format=%ci $STABLE_SHA)
 COMMITSINCESTABLE=$(git rev-list $STABLE_SHA..HEAD --count --since="$SHA_DATE")
 CURRENT_VERSION=$(node -p -e "require('./package.json').version")
+
+# Make the version number
 if [[ ($CIRCLE_BRANCH != "stable" && $GITHUB_REF_NAME != "stable") && $COMMITSINCESTABLE -gt 0 ]]; then
+  echo "This is not a stable branch, but there are commits since the last stable release. Setting beta version."
   NEW_VERSION="${CURRENT_VERSION}-beta.${COMMITSINCESTABLE}"
 else
+  echo "This is a stable branch. Setting stable version."
   NEW_VERSION=${CURRENT_VERSION/0/$COMMITSINCESTABLE}
 fi
+
 echo "Version: $NEW_VERSION"
 echo "Current version: $CURRENT_VERSION"
 if [[ $COMMITSINCESTABLE -gt 0 ]]; then
@@ -19,6 +22,7 @@ else
   echo "Version unchanged, commits since stable is ${COMMITSINCESTABLE}"
 fi
 
+# Make it a environment variable
 if [[ -z "${GITHUB_ENV}" ]]; then
   echo "APP_VERSION=$(node -p -e 'require("./package.json").version')" >>$GITHUB_ENV
 elif [[ -z "${BASH_ENV}" ]]; then
