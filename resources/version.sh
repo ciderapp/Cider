288cf36648010f77866de732111007be1d420456
Core <64542347+coredev-uk@users.noreply.github.com>
2022-08-04 06:31:03 +0100

    fix

--- resources/version.sh
+++ resources/version.sh
@@ -5,12 +5,14 @@ SHA_DATE=$(git show -s --format=%ci $STABLE_SHA)
 COMMITSINCESTABLE=$(git rev-list $STABLE_SHA..HEAD --count --since="$SHA_DATE")
 CURRENT_VERSION=$(node -p -e "require('./package.json').version")
 LATEST_VERSION=$(curl -s https://api.github.com/repos/ciderapp/cider-releases/releases/latest | grep tag_name | cut -d '"' -f 4 | sed 's/v//' | xargs)
+GITHUB_REF_NAME="main"
 
 # Set the version number for commits on main branch
 if [[ ($CIRCLE_BRANCH == "main" || $GITHUB_REF_NAME == "main") && $COMMITSINCESTABLE -gt 0 ]]; then
   NEW_VERSION="${CURRENT_VERSION}-beta.${COMMITSINCESTABLE}"
-
-  if [[ $GITHUB_REF_NAME == "main" && $LATEST_VERSION != $NEW_VERSION ]]; then
+  
+  # This is shit
+  if [[ $GITHUB_REF_NAME == "main" && $CURRENT_VERSION == $NEW_VERSION ]]; then
     echo "No version could be made. Picking latest tag."
     NEW_VERSION="${LATEST_VERSION}"
   fi
