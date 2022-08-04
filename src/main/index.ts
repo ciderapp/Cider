8a412f409394e68171637f88fa23bc8094d400c8
coredev-uk <coredev-uk@users.noreply.github.com>
2022-08-04 20:25:03 +0000

    chore: Prettified Code  [ci skip]

--- src/main/index.ts
+++ src/main/index.ts
@@ -27,7 +27,7 @@ Sentry({
 new Store();
 const Cider = new AppEvents();
 const CiderPlug = new Plugins();
-            console.log("hi")
+console.log("hi");
 
 /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
  * App Event Handlers
