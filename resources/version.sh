a97340c7cc1ad75506d74b92aec5ee518140e423
Core <64542347+coredev-uk@users.noreply.github.com>
2022-08-04 05:41:22 +0100

    Fallback for macos builds

--- resources/version.sh
+++ resources/version.sh
@@ -4,10 +4,15 @@ STABLE_SHA=$(curl -s https://api.github.com/repos/ciderapp/Cider/branches/stable
 SHA_DATE=$(git show -s --format=%ci $STABLE_SHA)
 COMMITSINCESTABLE=$(git rev-list $STABLE_SHA..HEAD --count --since="$SHA_DATE")
 CURRENT_VERSION=$(node -p -e "require('./package.json').version")
+LATEST_VERSION=$(curl -s https://api.github.com/repos/ciderapp/cider-releases/releases/latest | grep tag_name | cut -d '"' -f 4 | sed 's/v//' | sed 's/\-/./g' | xargs)
 
 # Set the version number for commits on main branch
 if [[ ($CIRCLE_BRANCH == "main" || $GITHUB_REF_NAME == "main") && $COMMITSINCESTABLE -gt 0 ]]; then
   NEW_VERSION="${CURRENT_VERSION}-beta.${COMMITSINCESTABLE}"
+
+  if [[ $GITHUB_REF_NAME != "" && $LATEST_VERSION != $NEW_VERSION ]]; then
+    NEW_VERSION="${LATEST_VERSION}"
+  fi
 
   # Update the version in package.json
   if [[ $RUNNER_OS == "macOS" ]]; then
