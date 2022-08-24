6d9bb8fa9538f3766c100e04a6cd777279986c69
Core <core@coredev.uk>
2022-08-24 07:32:12 +0100

    moved check

--- resources/version.sh
+++ resources/version.sh
@@ -2,11 +2,11 @@
 
 # Setup the variables needed
 if [[ $GH_REQUEST_TOKEN != "" ]]; then
-  STABLE_SHA=$(curl -H "Authorization: token ${GH_REQUEST_TOKEN}" -s https://api.github.com/repos/ciderapp/Cider/branches/stable | grep '"sha"' | head -1 | cut -d '"' -f 4)
+	STABLE_SHA=$(curl -H "Authorization: token ${GH_REQUEST_TOKEN}" -s https://api.github.com/repos/ciderapp/Cider/branches/stable | grep '"sha"' | head -1 | cut -d '"' -f 4)
 elif [[ $GITHUB_TOKEN != "" ]]; then
-  STABLE_SHA=$(curl -H "Authorization: token ${GITHUB_TOKEN}" -s https://api.github.com/repos/ciderapp/Cider/branches/stable | grep '"sha"' | head -1 | cut -d '"' -f 4)
+	STABLE_SHA=$(curl -H "Authorization: token ${GITHUB_TOKEN}" -s https://api.github.com/repos/ciderapp/Cider/branches/stable | grep '"sha"' | head -1 | cut -d '"' -f 4)
 else
-  STABLE_SHA=$(curl -s https://api.github.com/repos/ciderapp/Cider/branches/stable | grep '"sha"' | head -1 | cut -d '"' -f 4)
+	STABLE_SHA=$(curl -s https://api.github.com/repos/ciderapp/Cider/branches/stable | grep '"sha"' | head -1 | cut -d '"' -f 4)
 fi
 
 SHA_DATE=$(git show -s --format=%ci $STABLE_SHA)
@@ -14,19 +14,15 @@ COMMIT_SINCE_STABLE=$(git rev-list $STABLE_SHA..HEAD --count --since="$SHA_DATE"
 CURRENT_VERSION=$(node -p -e "require('./package.json').version")
 
 # Set the version number for commits on main branch
-if [[ ($CIRCLE_BRANCH == "main" || $GITHUB_REF_NAME == "main") && $COMMIT_SINCE_STABLE -gt 0 ]]; then
-  NEW_VERSION="${CURRENT_VERSION}-beta.${COMMIT_SINCE_STABLE}"
-
-  # Not sure why this is needed, but it is
-  if [[ $() != $COMMIT_SINCE_STABLE ]]; then
-    # Update the version in package.json
-    if [[ $RUNNER_OS == "macOS" ]]; then
-      sed -i "" -e "s/$CURRENT_VERSION/$NEW_VERSION/" package.json
-    else
-      sed -i "0,/$CURRENT_VERSION/s//$NEW_VERSION/" package.json
-    fi
-      echo $NEW_VERSION
-  fi
+if [[ ($CIRCLE_BRANCH == "main" || $GITHUB_REF_NAME == "main") && $COMMIT_SINCE_STABLE -gt 0 && $(node -p -e "require('./package.json').version" | cut -d '.' -f 4) != $COMMIT_SINCE_STABLE ]]; then
+	NEW_VERSION="${CURRENT_VERSION}-beta.${COMMIT_SINCE_STABLE}"
+	# Update the version in package.json
+	if [[ $RUNNER_OS == "macOS" ]]; then
+		sed -i "" -e "s/$CURRENT_VERSION/$NEW_VERSION/" package.json
+	else
+		sed -i "0,/$CURRENT_VERSION/s//$NEW_VERSION/" package.json
+	fi
+	echo $NEW_VERSION
 else
-  echo $CURRENT_VERSION
+	echo $CURRENT_VERSION
 fi
