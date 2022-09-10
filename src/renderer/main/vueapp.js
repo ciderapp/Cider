8466dd65de0da3629e8bd27a0c3c8efa9d3fa43b
coredev-uk <coredev-uk@users.noreply.github.com>
2022-09-10 23:49:28 +0000

    chore: Prettified Code  [ci skip]

--- src/renderer/main/vueapp.js
+++ src/renderer/main/vueapp.js
@@ -4539,7 +4539,7 @@ const app = new Vue({
                 if (app.mk.nowPlayingItem.relationships.artists.data[0].id) {
                   app.appRoute(`artist/${app.mk.nowPlayingItem.relationships.artists.data[0].id}`);
                 } else {
-                  const id = await MusicKitInterop.fetchPrimaryArtist("id")
+                  const id = await MusicKitInterop.fetchPrimaryArtist("id");
                   app.appRoute(`artist/${id}`);
                 }
               },
