6f51759b408eda3a35edc63699458dc116726a16
Charles Surett <surettcharles@gmail.com>
2022-09-15 01:01:01 -0400

    Add song.link

--- src/main/plugins/discordrpc.ts
+++ src/main/plugins/discordrpc.ts
@@ -237,6 +237,7 @@ export default class DiscordRPC {
       activity.buttons = [
         { label: "Listen on Cider", url: attributes.url.cider },
         { label: "View on Apple Music", url: attributes.url.appleMusic },
+        { label: "Song.link", url: 'https://song.link/i/' + attributes.songId }
       ]; //To change attributes.url => preload/cider-preload.js
     }
 
