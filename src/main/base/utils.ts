f99bd11d0525c2b28628f292d708a51136748cc2
maikirakiwi <maikirakiwi@users.noreply.github.com>
2022-10-17 00:27:20 +0000

    chore: Prettified Code  [ci skip]

--- src/main/base/utils.ts
+++ src/main/base/utils.ts
@@ -84,20 +84,17 @@ export class utils {
    * @param url {string} URL param
    * @param opts {object} Other options
    */
-  static readonly _mirror: boolean = this.getStoreValue("advanced.experiments").includes('cider_mirror') ? true : false;
+  static readonly _mirror: boolean = this.getStoreValue("advanced.experiments").includes("cider_mirror") ? true : false;
   static fetch(url: string, opts = {}) {
     if (this._mirror === true) {
       if (url.includes("api.github.com/")) {
         return fetch(url.replace("api.github.com/", "mirror.api.cider.sh/v2/api/"), opts);
-      }
-      else if (url.includes("raw.githubusercontent.com/")) {
+      } else if (url.includes("raw.githubusercontent.com/")) {
         return fetch(url.replace("raw.githubusercontent.com/", "mirror.api.cider.sh/v2/raw/"), opts);
-      }
-      else {
+      } else {
         return fetch(url, opts);
       }
-    }
-    else {
+    } else {
       return fetch(url, opts);
     }
   }
